39ec4d797ea39f6fdd923df8881cf34149349a69
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-03-26 15:39:40 +0100

    aggiunta colonna

---
+++ MyProject/database/migrations/2021_03_26_141234_create_paintings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaintingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paintings', function (Blueprint $table) {
+            $table->id();
+            $table->string('author', 255);
+            $table->string('period', 255);
+            $table->string('image', 2048);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paintings');
+    }
+}
